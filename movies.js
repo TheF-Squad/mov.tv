c311776c049512a52c38d453419b5e01d52672d6
TheF-Squad <134857533+TheF-Squad@users.noreply.github.com>
2026-02-07 19:53:12 -0500

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,48 @@
+// js/movies.js
+const catalog = [
+  {
+    series: Bluey,
+    seasons:  ]3
+      {
+        seasonNumber: 1,
+        episodes:  ]52 
+          {
+            title: Magic Xylophone,
+            thumb: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgCvLCqwORneWxSQ5RJnCcndXa-A4Wsl3vCg&s,
+            url: bluey-2018-s-01e-01-magic-xylophone
+          },
+          {
+            title: "Episode 2",
+            thumb: "images/bigbuckbunny_s1e2jpg",
+            url: "https://archive.org/embed/Big_Buck_Bunny_Part2"
+          }
+        ]
+      },
+      {
+        seasonNumber: 2,
+        episodes: [
+          {
+            title: "Episode 1",
+            thumb: "images/bigbuckbunny_s2e1.jpg",
+            url: "https://archive.org/embed/Big_Buck_Bunny_S2E1"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    series: "Popeye the Sailor",
+    seasons: [
+      {
+        seasonNumber: 1,
+        episodes: [
+          {
+            title: "Episode 1",
+            thumb: "images/popeye_s1e1.jpg",
+            url: "https://archive.org/embed/Popeye_the_Sailor"
+          }
+        ]
+      }
+    ]
+  }
+];
